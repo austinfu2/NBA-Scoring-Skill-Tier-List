a21d071a3ec746093ab03317d008547da426408f
Austin <austinurraca@gmail.com>
2023-03-27 23:58:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,12 @@ from bs4 import BeautifulSoup
 import csv
 import re
 
-
 app = Flask(__name__)
 
 # get a list of all player image filenames in the player_faces folder
 player_images = os.listdir("static/player_faces")
 # shuffle the list of player image filenames
 random.shuffle(player_images)
-
-# loop over each imagebox and assign a random image to its src attribute
-with open("templates/index.html") as file:
-    soup = BeautifulSoup(file, 'html.parser')
 
 def get_player_name(player_id):
     with open('static/names.csv', encoding="utf-8") as csvfile:
@@ -42,34 +37,48 @@ def home():
     # save the updated HTML back to the file
     with open("templates/index.html", "w", encoding="utf-8") as output_file:
         output_file.write(str(soup))
-    return render_template("index.html", player_images=player_images)
 
-# loop over the dropzones and insert the player positions into the database
+    # Get the current page HTML
+    with open('templates/index.html', 'r') as f:
+        current_page_html = f.read()
+
+    return render_template('index.html', current_page_html=current_page_html)
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    # Get the data from the AJAX request
+    data = request.get_json()
+
+    # Connect to the database
+    conn = sqlite3.connect('mydatabase.db')
+    c = conn.cursor()
+
+    # Loop over the data and insert each record into the database
+    for record in data:
+        dropzone_id = record['dropzone_id']
+        data_image = record['data_image']
+        data_player_name = record['data_player_name']
+        c.execute("INSERT INTO player_real (dropzone_id, data_image, data_player_name) VALUES (?, ?, ?)", (dropzone_id, data_image, data_player_name))
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+    # Return a success response
+    return render_template("results.html")
+
 @app.route('/submit', methods=["GET", "POST"])
 def submit():
-    try:
-        # connect to the database
-        with sqlite3.connect('mydatabase.db') as conn:
-            # create a cursor object
-            cursor = conn.cursor()
-
-            for i in range(0, 10):
-                dropzone_id = f"dropzone-{i}"
-
-
-        return render_template("results.html", message="Data successfully saved to database.")
-    except Exception as e:
-        return render_template("results.html", message=f"Error: {e}")
+    return render_template("results.html")
 
 # define the route for the results page
 @app.route('/results')
 def results():
-    return render_template("templates/results.html")
+    return render_template("results.html")
 
 # add a context processor to make the player_images list available to all templates
-@app.context_processor
-def inject_player_images():
-    return dict(player_images=player_images)
+#@app.context_processor
+#def inject_player_images():
+#    return dict(player_images=player_images)
 
 if __name__ == "__main__":
     app.run(debug=True)
